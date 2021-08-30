d3d5adb9351de532e8d73fd7b9327eaff9c14f83
daovietanh190499 <daovietanhsky@gmail.com>
2021-08-30 09:26:13 +0700

    remove filter

--- sfm.py
+++ sfm.py
@@ -74,11 +74,6 @@ def to_ply(img_dir, point_cloud, colors, subfix = "_sparse.ply"):
     out_colors = colors.reshape(-1, 3)
     print(out_colors.shape, out_points.shape)
     verts = np.hstack([out_points, out_colors])
-    mean = np.mean(verts[:, :3], axis=0)
-    temp = verts[:, :3] - mean
-    dist = np.sqrt(temp[:, 0] ** 2 + temp[:, 1] ** 2 + temp[:, 2] ** 2)
-    indx = np.where(dist < np.mean(dist) + 300)
-    verts = verts[indx]
     ply_header = '''ply
 		format ascii 1.0
 		element vertex %(vert_num)d
